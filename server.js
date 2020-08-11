e2df8e11b2505431e697faa44f63f83b636723f4
Darius Matthews <DariustheDeveloper@gmail.com>
2020-08-11 11:00:45 -0500

    Implemented ES6 modules for node.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
 
 // initialize express
 const app = express();
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
+// REST ACTIONS {GET, POST, PUT, DELETE}
 // GET request at root
 app.get('/', (req, res) => {
   res.send('GET Working');
@@ -28,6 +29,7 @@ app.delete('/users', (req, res) => {
   res.send('DELETE Working. Deleted user at /users');
 })
 
+``
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on ${port}`))
