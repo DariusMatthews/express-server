1810b7bedd596a2be8bb10c9caada62476581561
Darius Matthews <DariustheDeveloper@gmail.com>
2020-08-10 23:41:43 -0500

    Added CRUD.

--- server.js
+++ server.js
@@ -2,9 +2,25 @@ const express = require('express');
 
 const app = express();
 
+// GET request at root
 app.get('/', (req, res) => {
-  res.send('We Working');
+  res.send('GET Working');
 });
+
+// POST request at root
+app.post('/', (req, res) => {
+  res.send('POST Working');
+});
+
+// PUT request at users
+app.put('/users', (req, res) => {
+  res.send('PUT Working. Add a user at /users');
+});
+
+// DELETE request at users
+app.delete('/users', (req, res) => {
+  res.send('DELETE Working. Deleted user at /users');
+})
 
 const port = process.env.PORT || 5000;
 
