2519491916f12cbc0bf33c1ce45d07b58562811f
Darius Matthews <DariustheDeveloper@gmail.com>
2020-08-13 14:58:44 -0500

    added mongodb.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import path from 'path';
+import mongodb from 'mongodb';
+// import path from 'path';
 
 // initialize express
 const app = express();
@@ -29,7 +30,7 @@ app.delete('/users', (req, res) => {
   res.send('DELETE Working. Deleted user at /users');
 })
 
-``
+  ``
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on ${port}`))
